31d40fafaef58d829a860399cef09db45d24ef69
EnduranceCode <endurancecode@gmail.com>
2021-02-13 20:03:02 +0000

    S02C21 - Use the template property

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -2,7 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  template: `
+  <div class="jumbotron">
+    <h3>In the Servers Component</h3>
+    <app-server></app-server>
+    <app-server></app-server>
+  </div>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
