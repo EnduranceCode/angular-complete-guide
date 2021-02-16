05c09511c768c8e3acb721169cf3eeeceee9538f
EnduranceCode <endurancecode@gmail.com>
2021-02-16 20:12:23 +0000

    S02 - Assignement 2

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -7,5 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular App';
-  name = 'EnduranceCode'
+  name = 'EnduranceCode';
+  userName = '';
+  inputFeedback: String;
+  buttonClickable: boolean = false;
+
+  onTextInput(event: Event) {
+    if ((<HTMLInputElement>event.target).value.trim().length != 0) {
+      this.inputFeedback = 'The value typed is: ' + this.userName;
+      this.buttonClickable = true;
+    } else {
+      this.inputFeedback = '';
+      this.buttonClickable = false;
+    }
+  }
+
+  onClickButton() {
+    this.userName = '';
+    this.inputFeedback = '';
+  }
 }
