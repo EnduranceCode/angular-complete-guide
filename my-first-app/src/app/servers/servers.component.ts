98dc1bb3787dd587d7574249b0c2858b25055bba
EnduranceCode <endurancecode@gmail.com>
2021-02-16 17:43:51 +0000

    S02C31 - Pass data with Event Binding

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -12,6 +12,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer: boolean = false;
   serverCreationStatus = "No server was created!";
+  serverName = "";
 
   constructor() {
     setTimeout(() => { this.allowNewServer = true }, 3000);
@@ -22,5 +23,10 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus = "Server was created!";
+  }
+
+  onUpdateServerName(event: Event) {
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
